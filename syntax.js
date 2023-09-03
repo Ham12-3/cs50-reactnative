ee4556ae362c2ef36cf64b2ee1d633ae89b3be9b
Abdulhamid <mobolaji2309@gmail.com>
2023-09-03 17:37:20 +0100

    updating

--- syntax.js
+++ syntax.js
@@ -16,3 +16,16 @@ arr[2]();
 for (let i = 0; i < arr.length; i++) {
   console.log(arr[i]);
 }
+
+//coercion
+const x = 42;
+const explicit = String(x);
+const implicit = x + "";
+console.log(typeof x);
+console.log(typeof 42);
+console.log(typeof null);
+
+//falsy value and truthy value
+
+// falsy value
+// unndefined , null , false, +0 ,-0 , Nan
