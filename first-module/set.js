614622112ffbee213b6d4208e13a6330b2db6e94
Abdulhamid <mobolaji2309@gmail.com>
2023-09-04 17:32:40 +0100

    updating

---
+++ first-module/set.js
@@ -0,0 +1,22 @@
+class Set {
+  constructor(arr) {
+    this.arr = arr;
+  }
+  add(val) {
+    if (!this.has(val)) this.arr.push(val);
+  }
+  delete(val) {
+    this.arr = this.arr.filter((x) => x !== val);
+  }
+  has(val) {
+    return this.arr.includes(val);
+  }
+}
+
+const s = new Set([1, 2, 3, 4]);
+
+s.add(1);
+s.add(1);
+s.add(1);
+
+console.log(s);
