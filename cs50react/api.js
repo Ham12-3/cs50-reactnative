f09467014d928c8ed71aa29e2b94a06d03cbf954
Abdulhamid <mobolaji2309@gmail.com>
2023-09-09 07:20:01 +0100

    updating

--- cs50react/api.js
+++ cs50react/api.js
@@ -1,5 +1,22 @@
+const processContact = (contact) => ({
+  name: `${contact.name.first}  ${contact.name.last}`,
+  phone: contact.phone,
+});
+
 export const fetchUsers = async () => {
   const reponse = await fetch("https://randomuser.me/api?results=50&nat=us");
   const { results } = await Response.json();
-  return results;
+  return results.map(processContact);
 };
+
+// http method
+/* 
+Get
+Post
+*/
+// LoginScreen 
+
+import { TextInput } from "react-native";
+
+<TextInput placeholder="username" value={this.state.username} onChange={this.handleUsernameUpdate}/>
+<TextInput placeholder="password" value={this.state.password}/>
