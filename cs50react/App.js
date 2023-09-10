a7cf250c6d2f25958a4d4fc2d561bc8e621f6365
Abdulhamid <mobolaji2309@gmail.com>
2023-09-10 12:28:01 +0100

    updating

--- cs50react/App.js
+++ cs50react/App.js
@@ -1,19 +1,58 @@
-<script src="http://localhost:8097"></script>;
 import { StatusBar } from "expo-status-bar";
+import { useState, useEffect } from "react";
 import { StyleSheet, Text, View } from "react-native";
 // import Count, { num } from "./Count";
+import * as Location from "expo-location";
+import MapView, { PROVIDER_GOOGLE, Marker } from "react-native-maps";
 
-import Expo from "expo";
 export default function App() {
   console.log("Hello");
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        setErrorMsg("Permission to access location was denied");
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+      console.log(location);
+    })();
+  }, []);
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
-      <Expo.MapView
+      {/* <Expo.MapView
         style={{ flex: 1 }}
         provider={Expo.MapView.PROVIDER_GOOGLE}
-      />
+      /> */}
+
+      {location ? (
+        <MapView
+          size={20}
+          initialRegion={{
+            latitude: location.coords.latitude,
+            longitude: location.coords.longitude,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+          provider={PROVIDER_GOOGLE}
+          style={{
+            width: 500,
+            height: 500,
+          }}
+          loadingEnabled={true}
+        >
+          <Marker coordinate={location.coords} title="You are here" />
+        </MapView>
+      ) : (
+        <View />
+      )}
     </View>
   );
 }
