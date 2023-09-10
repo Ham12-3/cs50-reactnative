14d038800942a80759a7e6aad62b14d632ca512b
Abdulhamid <mobolaji2309@gmail.com>
2023-09-10 11:11:32 +0100

    updating

--- cs50react/App.js
+++ cs50react/App.js
@@ -1,14 +1,19 @@
 <script src="http://localhost:8097"></script>;
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-import Count, { num } from "./Count";
+// import Count, { num } from "./Count";
+
+import Expo from "expo";
 export default function App() {
   console.log("Hello");
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
-      <Count count={num} />
+      <Expo.MapView
+        style={{ flex: 1 }}
+        provider={Expo.MapView.PROVIDER_GOOGLE}
+      />
     </View>
   );
 }
