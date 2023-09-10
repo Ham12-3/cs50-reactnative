19cdb72d698e83171433f3e3145321b5effcaa57
Abdulhamid <mobolaji2309@gmail.com>
2023-09-10 09:09:11 +0100

    updating

--- cs50react/Count.js
+++ cs50react/Count.js
@@ -1,87 +1,87 @@
-import { View, Text, StyleSheet } from "react-native";
-import React from "react";
-import PropTypes from "prop-types";
-const styles = StyleSheet.create({
-  text: { fontSize: 72 },
-});
-const Count = ({ count }) => {
-  return (
-    <View>
-      <Text style={styles.text}>{count}</Text>
-    </View>
-  );
-};
-Count.propTypes = {
-  count: PropTypes.number.isRequired,
-};
-// static propTypes = {
-//     count: PropTypes.number.isRequired
-// }
-export const num = 50;
+// import { View, Text, StyleSheet } from "react-native";
+// import React from "react";
+// import PropTypes from "prop-types";
+// const styles = StyleSheet.create({
+//   text: { fontSize: 72 },
+// });
+// const Count = ({ count }) => {
+//   return (
+//     <View>
+//       <Text style={styles.text}>{count}</Text>
+//     </View>
+//   );
+// };
+// Count.propTypes = {
+//   count: PropTypes.number.isRequired,
+// };
+// // static propTypes = {
+// //     count: PropTypes.number.isRequired
+// // }
+// export const num = 50;
 
-export default Count;
+// export default Count;
 
-// //react-navigation
-// createStackNavigator ({
-//   route: ''
-// })
-
-// const AppNavigator = createSwitchNavigator({
-//   AddContact:AddCcontactScreen,
-//   ContactList:ContactListScreen,
-
-// {
-//   tabBarOPtions {
-//     activeTintColor:
-//   }
-// }
-// // tabnavigator
-// createBottomNavigstor
-// atb
-// headerTintCololr
+// // //react-navigation
+// // createStackNavigator ({
+// //   route: ''
 // // })
-// push
-// createNativeStackNavigator
-// screenProps
-{
-  /* <AppNavigator screenProps={{ contact: "name" }} />; */
-}
 
-// this.props.navigation.navigate('Ádd contacts')
+// // const AppNavigator = createSwitchNavigator({
+// //   AddContact:AddCcontactScreen,
+// //   ContactList:ContactListScreen,
 
-// this.props.navigation.getparam();
-// stackNaviagtor
-// headerTitle
-// header
-// static navigationOptions {
-//   headerTitle:
-//   headerRight:
-
-// }
-// this.props.naviagtion.goBack
-// static navigationOptions =({navigation })=> {
-//   return {
-//     headerttitle: ""
-// headerRight:''
-// headerLeft:''
-//   }
+// // {
+// //   tabBarOPtions {
+// //     activeTintColor:
+// //   }
+// // }
+// // // tabnavigator
+// // createBottomNavigstor
+// // atb
+// // headerTintCololr
+// // // })
+// // push
+// // createNativeStackNavigator
+// // screenProps
+// {
+//   /* <AppNavigator screenProps={{ contact: "name" }} />; */
 // }
 
-// Data Async Await
+// // this.props.navigation.navigate('Ádd contacts')
 
-componentDidMount(() => {
-  fetch("https://randomuser.me/api?results=50&nat=us")
-    .then((response) => response.json())
-    .then((results) => this.setState({ contacts: results.results }));
-});
+// // this.props.navigation.getparam();
+// // stackNaviagtor
+// // headerTitle
+// // header
+// // static navigationOptions {
+// //   headerTitle:
+// //   headerRight:
 
-import { fetchUsers } from "./api";
-componentDidMount(() => {
-  this.egtUsers();
-  fetchUsers().then((results) => this.setState({ contacts: results }));
+// // }
+// // this.props.naviagtion.goBack
+// // static navigationOptions =({navigation })=> {
+// //   return {
+// //     headerttitle: ""
+// // headerRight:''
+// // headerLeft:''
+// //   }
+// // }
 
-  const getUsers = async () => {
-    const results = await fetchUsers();
-    this.setState({ contacts: results });
-  };
-});
+// // Data Async Await
+
+// componentDidMount(() => {
+//   fetch("https://randomuser.me/api?results=50&nat=us")
+//     .then((response) => response.json())
+//     .then((results) => this.setState({ contacts: results.results }));
+// });
+
+// import { fetchUsers } from "./api";
+// componentDidMount(() => {
+//   this.egtUsers();
+//   fetchUsers().then((results) => this.setState({ contacts: results }));
+
+//   const getUsers = async () => {
+//     const results = await fetchUsers();
+//     this.setState({ contacts: results });
+//   };
+// });
--- cs50react/api.js
+++ cs50react/api.js
@@ -1,56 +1,61 @@
-const processContact = (contact) => ({
-  name: `${contact.name.first}  ${contact.name.last}`,
-  phone: contact.phone,
-});
+// const processContact = (contact) => ({
+//   name: `${contact.name.first}  ${contact.name.last}`,
+//   phone: contact.phone,
+// });
 
-export const fetchUsers = async () => {
-  const reponse = await fetch("https://randomuser.me/api?results=50&nat=us");
-  const { results } = await Response.json();
-  return results.map(processContact);
-};
+// export const fetchUsers = async () => {
+//   const reponse = await fetch("https://randomuser.me/api?results=50&nat=us");
+//   const { results } = await Response.json();
+//   return results.map(processContact);
+// };
 
-// http method
-/* 
-Get
-Post
-*/
-// LoginScreen
+// // http method
+// /*
+// Get
+// Post
+// */
+// // LoginScreen
 
-import { TextInput } from "react-native";
+// import { TextInput } from "react-native";
 
-state = {
-  username: "",
-  password: "",
-};
-handleUsernameUpdate = (username) => {
-  this.setState({ username });
-};
-handlePasswordUpdate = (password) => {
-  this.setState({ password });
-};
+// state = {
+//   username: "",
+//   password: "",
+// };
+// handleUsernameUpdate = (username) => {
+//   this.setState({ username });
+// };
+// handlePasswordUpdate = (password) => {
+//   this.setState({ password });
+// };
 
-<>
-  <TextInput
-    placeholder="username"
-    value={this.state.username}
-    onChangeText={this.handleUsernameUpdate}
-  />
-  <TextInput
-    placeholder="password"
-    value={this.state.password}
-    onChangeText={this.handlePasswordUpdate}
-  />
-</>;
-// https mehod
-fetch("http://localhost:8000", {
-  method: "POST",
-  headers: { "content-type": "application/json" },
-  body: JSON.stringify({
-    username: this.state.username,
-    password: this.state.password,
-  }),
-});
+// <>
+//   <TextInput
+//     placeholder="username"
+//     value={this.state.username}
+//     onChangeText={this.handleUsernameUpdate}
+//   />
+//   <TextInput
+//     //   secureTextEntry
+//     //   autoCapitalize={none}
+//     placeholder="password"
+//     value={this.state.password}
+//     onChangeText={this.handlePasswordUpdate}
+//   />
+// </>;
+// // https mehod
+// fetch("http://localhost:8000", {
+//   method: "POST",
+//   headers: { "content-type": "application/json" },
+//   body: JSON.stringify({
+//     username: this.state.username,
+//     password: this.state.password,
+//   }),
+// });
 
-// HTTP response code
+// // HTTP response code
 
-// node and express.js
+// // node and express.js
+// if (Response.ok) {
+// }
+// // this.state.err
