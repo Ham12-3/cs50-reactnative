db254c763528c4e0e03d2a4af33bdf76db2214de
Abdulhamid <mobolaji2309@gmail.com>
2023-09-14 07:09:31 +0100

    update

--- cs50react/reducer.js
+++ cs50react/reducer.js
@@ -1,4 +1,25 @@
-const reducer = (state, update) => ({
-  ...state,
-  update,
-});
+const merge = (prev, next) => Object.assign({}, prev, next);
+
+const reducer = (state, update) => merge(state, update);
+
+let state = {};
+state = reducer(state, { foo: "foo" });
+
+class Store {
+  constructor(reducer, initialState) {
+    this.reducer = reducer;
+    this.state = initialState;
+  }
+  getState() {
+    return this.state;
+  }
+  dispatch(update) {
+    this.state = this.reducer(this.state, update);
+  }
+}
+
+const store = new Store(reducer);
+store.dispatch({ bar: "bar" });
+store.dispatch({ baz: "baz" });
+store.dispatch({ foo: "foo" });
+console.log(store.getState());
